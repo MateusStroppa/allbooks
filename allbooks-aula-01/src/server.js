f193f4739b223955a6922e2d14d5bd3bd977120d
Mateus Stroppa <mateus_stro@hotmail.com>
2025-07-28 10:26:12 -0300

    Altera o server.js

--- allbooks-aula-01/src/server.js
+++ allbooks-aula-01/src/server.js
@@ -18,10 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas vindas à API do Allbooks")
-  console.log("API disponível através da URL http://localhost:8000")
-=======
+  console.log("Olá! Essa é a API do Allbooks")
   console.log("Acesse essa API em http://localhost:8000")
->>>>>>> abfdaa7943e5518eceebe5c040afa0b5ce16543f
 })
